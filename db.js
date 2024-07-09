9e7a15f09595fad0cea252bdf4222729db59f8e7
FutureTechMaestro <bmdamanulla111999@gmail.com>
2024-07-09 15:54:47 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+const beenv  = require('dotenv');
+
+dotenv.config();
+const connection = mysql.createConnection({
+    host : process.env.DB_host,
+    user : process.env.DB_user,
+    password :process.env.DB_password,
+    database : process.env.DB_database,
+    port: process.env.DB_port,
+
+});
+
+
+connection.connect((err)=>{
+    if(err)
+    {
+        console.error("Error connection to the mysql,err.message");
+        process.exit(1);
+    }
+    else{
+       console.log("Connected to the mysql");
+    }
+});
+module.exports = connection;
